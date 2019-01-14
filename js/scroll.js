4d27be41aff582369bbd3577277d540dddb1f5bc
Khanya Keswa <khanya@khanyas-mbp-2.lan>
2019-01-14 09:49:43 -0500

    skills_animation refined

--- js/scroll.js
+++ js/scroll.js
@@ -8,7 +8,7 @@ function doSomething(scroll_pos) {
   console.log('scroll', scroll_pos, window.innerHeight)
   // navbar.style.transform = `translateX(${scroll_pos}px)`
   // Do something with the scroll position
-  const isScrolledPastTopSection = scroll_pos >= (0.5 * (window.innerHeight));
+  const isScrolledPastTopSection = scroll_pos >= (0.5 * window.innerHeight);
   if (!isCollapsed && isScrolledPastTopSection) {
     document.body.classList.add('collapsed');
     isCollapsed = true;
@@ -19,6 +19,21 @@ function doSomething(scroll_pos) {
     isCollapsed = false;
     console.log('uncollapsing');
   }
+
+  const isScrolledPastAboutMe = scroll_pos >= (2 * window.innerHeight);
+  if (!LottieAnimations.hasSkillsGraph1Played && isScrolledPastAboutMe){
+    LottieAnimations.skillsGraph1.play();
+    LottieAnimations.hasSkillsGraph1Played = true;
+  }
+  if (!LottieAnimations.hasSkillsGraph2Played && isScrolledPastAboutMe){
+    LottieAnimations.skillsGraph2.play();
+    LottieAnimations.hasSkillsGraph2Played = true;
+  }
+  if (!LottieAnimations.hasSkillsGraph3Played && isScrolledPastAboutMe){
+    LottieAnimations.skillsGraph3.play();
+    LottieAnimations.hasSkillsGraph3Played = true;
+  }
+
 }
 
 window.addEventListener('scroll', function(e) {
--- js/skills-animations.js
+++ js/skills-animations.js
@@ -1,7 +1,48 @@
-var animation = lottie.loadAnimation({
-  wrapper: ,
-  renderer: 'svg', 
-  loop: false,
-  autoplay: true,
-  path: 'skills_graph_1.json'
+'use strict';
+
+const LottieAnimations = {
+  skillsGraph1: lottie.loadAnimation({
+    container: document.querySelector('.chart.UI-UX'),
+    isFrame: true,
+    autoplay: false,
+    renderer: 'svg',
+    path: '/Animations/skills_graph_1.json'
+  }),
+
+  skillsGraph2: lottie.loadAnimation({
+    container: document.querySelector('.chart.Industrial'),
+    isFrame: true,
+    autoplay: false,
+    renderer: 'svg',
+    path: '/Animations/skills_graph_2.json'
+  }),
+
+  skillsGraph3: lottie.loadAnimation({
+    container: document.querySelector('.chart.misc'),
+    isFrame: true,
+    autoplay: false,
+    renderer: 'svg',
+    path: '/Animations/skills_graph_3.json'
+  }),
+  hasSkillsGraph1Played: false,
+  hasSkillsGraph2Played: false,
+  hasSkillsGraph3Played: false
+}
+
+LottieAnimations.skillsGraph1.addEventListener('enterFrame', function(event) {
+  if (event.currentTime >= event.totalTime - 2) {
+    LottieAnimations.skillsGraph1.pause()
+  }
+})
+
+LottieAnimations.skillsGraph2.addEventListener('enterFrame', function(event) {
+  if (event.currentTime >= event.totalTime - 2) {
+    LottieAnimations.skillsGraph2.pause()
+  }
+})
+
+LottieAnimations.skillsGraph3.addEventListener('enterFrame', function(event) {
+  if (event.currentTime >= event.totalTime - 2) {
+    LottieAnimations.skillsGraph3.pause()
+  }
 })
